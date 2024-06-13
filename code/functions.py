d58312b64544ef5aca6c85981068ce084614975c
User <vito@pcpool25>
2024-06-13 22:34:21 +0200

    0613 backup

--- code/functions.py
+++ code/functions.py
@@ -8,24 +8,28 @@ import torch
 import clip
 from PIL import Image
 from tqdm import tqdm
+from skimage.measure import label, regionprops
 
-def samplot(image, mask_generator, ax=None):
+def samplot(image, mask_generator, label=None, ax=None):
     '''
     testing 
     '''
     masks = mask_generator.generate(image)
-    plt.figure(figsize=(20,20))
-    plt.imshow(image)
     if ax:
-        show_anns_mod_ax(masks, ax)
+        ax.imshow(image)  
+        show_anns_mod_ax(masks, ax, label)
+        ax.axis('off')
+        ax.set_title(f'Number of masks: {len(masks)}')
     else:
-        show_anns_mod(masks)
-    plt.axis('off')
-    plt.show() 
-    plt.figure(figsize=(20,20))
-    plt.imshow(image)
-    plt.axis('off')
-    plt.show() 
+        fig, ax=plt.subplots(1,2,figsize=(20,20))
+        ax[0].imshow(image)
+        ax[0].axis('off')
+        ax[1].imshow(image)
+        show_anns_mod_ax(masks, ax[1], label)
+        ax[1].axis('off')
+        plt.title(f'Number of masks: {len(masks)}')
+        plt.tight_layout()
+        plt.show()
     return masks
 def show_mask(mask, ax, random_color=False):
     if random_color:
@@ -62,7 +66,7 @@ def show_anns(anns):
         img[m] = color_mask
     ax.imshow(img)
 
-def show_anns_mod(anns):
+def show_anns_mod(anns, label=None):
     if len(anns) == 0:
         return
     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
@@ -76,15 +80,19 @@ def show_anns_mod(anns):
         m = ann['segmentation']
         color_mask = np.concatenate([np.random.random(3), [0.35]])
         img[m] = color_mask
-        ax.text(ann['point_coords'][0][0],ann['point_coords'][0][1], f'{i}')
+        if label:
+            ax.text(ann['point_coords'][0][0],ann['point_coords'][0][1], f'{i}')
         i+=1
 
     ax.imshow(img)
 
-def show_anns_mod_ax(anns, ax, label=None):
+def show_anns_mod_ax(anns, ax, label=None, sort=True):
     if len(anns) == 0:
         return
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    if sort:
+        sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    else:
+        sorted_anns=anns
     #ax = plt.gca()
     ax.set_autoscale_on(False)
 
@@ -213,3 +221,177 @@ def clipzeroshot(img, masks, zeroshot_weights, crop_size, target_index, model, D
                 most_probables.append(np.nan)
                 probabilities.append(np.nan)
     return clip_top1_probability_mask, clip_probability_mask, most_probables, probabilities
+
+def crop_to_valid(array):
+    sum_array=np.sum(array, axis=2)
+    valid_mask=[(sum_array!=0)&(sum_array!=765)]
+    notzero_indices = np.nonzero(valid_mask[0])
+    if len(notzero_indices[0])>0:
+        x_min = np.min(notzero_indices[0]).astype(int)# with max to avoid cropping border to get negative value
+        x_max = np.max(notzero_indices[0]).astype(int)# with min to avoid crossing image border
+        y_min = np.min(notzero_indices[1]).astype(int)
+        y_max = np.max(notzero_indices[1]).astype(int)
+    return array[x_min:x_max, y_min:y_max,:]
+def crop_valid_no_edge(array):
+    sum_array=np.sum(array, axis=2)
+    valid_mask=[(sum_array!=0)&(sum_array!=765)]
+
+    shift=1
+    test=np.where(np.sum(valid_mask[0],axis=0)==(np.sum(valid_mask[0],axis=0).max()-shift))
+    while len(test[0])<2:
+        shift+=1
+        test=np.where(np.sum(valid_mask[0],axis=0)==(np.sum(valid_mask[0],axis=0).max()-shift))
+    sizes=test[0][1:]-test[0][:-1]
+
+    while not np.any(sizes>=(np.sum(valid_mask[0],axis=0).max()-shift)):
+        shift+=1
+        test=np.where(np.sum(valid_mask[0],axis=0)==(np.sum(valid_mask[0],axis=0).max()-shift))
+        sizes=test[0][1:]-test[0][:-1]
+    
+    x_idx=np.nonzero(sizes>=(np.sum(valid_mask[0],axis=0).max()-shift))[0]
+    x_min,x_max=test[0][x_idx],test[0][x_idx+1]
+
+    shift=1
+    test=np.where(np.sum(valid_mask[0],axis=1)==(np.sum(valid_mask[0],axis=1).max()-shift))
+    while len(test[0])<2:
+        shift+=1
+        test=np.where(np.sum(valid_mask[0],axis=1)==(np.sum(valid_mask[0],axis=1).max()-shift))
+    sizes=test[0][1:]-test[0][:-1]
+
+    while not np.any(sizes>=(np.sum(valid_mask[0],axis=1).max()-shift)):
+        shift+=1
+        test=np.where(np.sum(valid_mask[0],axis=1)==(np.sum(valid_mask[0],axis=1).max()-shift))
+        sizes=test[0][1:]-test[0][:-1]
+    
+    y_idx=np.nonzero(sizes>=(np.sum(valid_mask[0],axis=1).max()-shift))[0]
+    y_min,y_max=test[0][y_idx],test[0][y_idx+1]
+    return y_min[0],y_max[0],x_min[0],x_max[0]
+
+def clean_mask(mask):
+    labels = label(mask)
+    l = len(np.unique(labels))
+    if l > 2:
+        #get area
+        regions = regionprops(labels)
+        # Sort regions by area
+        sorted_regions = sorted(regions, key=lambda x: x.area, reverse=True)
+        return (labels==sorted_regions[0].label).astype(np.uint8)
+    else:
+        return mask
+    
+
+def crop_fnc(input, para_in):
+    '''
+    crop size
+    i
+    j
+    '''
+    para={'crop size': 2048,
+          'i':0,
+          'j':0}
+    para.update(para_in)
+    crop_size=para.get('crop size')
+    i=para.get('i')
+    j=para.get('j')
+    output = input[crop_size*i:crop_size*(i+1),crop_size*j:crop_size*(j+1),:]
+    return output
+def gaussian_fnc(input, para_in):
+    para={'kernel size': 3}
+    para.update(para_in)
+    k=para.get('kernel size')
+
+    output = cv2.GaussianBlur(input, (k, k), k/6) 
+    return output
+
+def clahe_fnc(input, para_in):
+    para={'clahe window': 50,
+            'clip limit': 4
+            }
+    para.update(para_in)
+
+    clahe_ws=para.get('clahe window')
+    clip_limit=para.get('clip limit')
+
+    lab_image = cv2.cvtColor(input, cv2.COLOR_RGB2LAB)
+    l_channel, a_channel, b_channel = cv2.split(lab_image)
+    xysize=input.shape[:-1]
+    
+    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=(xysize[0]//clahe_ws,xysize[1]//clahe_ws))#84, 3-512
+    cl_channel = clahe.apply(l_channel)
+
+    merged_lab = cv2.merge([cl_channel, a_channel, b_channel])
+    output = cv2.cvtColor(merged_lab, cv2.COLOR_LAB2RGB)
+    return output
+
+def Lpull_fnc(input, para_in):
+    para={'thres': 60,
+          'pull': 60
+          }
+    para.update(para_in)
+
+    thres=para.get('thres')
+    pull=para.get('pull')
+
+    lab_image = cv2.cvtColor(input, cv2.COLOR_RGB2LAB)
+    l_channel, a_channel, b_channel = cv2.split(lab_image)
+    cl_channel = l_channel.copy()
+    cl_channel[cl_channel<thres]+=pull
+
+    merged_lab = cv2.merge([cl_channel, a_channel, b_channel])
+    output = cv2.cvtColor(merged_lab, cv2.COLOR_LAB2RGB)
+    return output
+
+def downsample_fnc(input, para_in):
+    para={'fxy':2,
+          'inter_area': None}
+    para.update(para_in)
+
+    fxy=para.get('fxy')
+    if para.get('inter_area'):
+        output=cv2.resize(input, (0, 0), fx = fxy, fy = fxy, interpolation = cv2.INTER_AREA)
+    else:
+        output=cv2.resize(input, (0, 0), fx = fxy, fy = fxy)
+    return output
+
+
+def load_roulette(input, process, para_in):
+    if process=='Crop':
+        output=crop_fnc(input, para_in)
+    elif process=='Gaussian':
+        output = gaussian_fnc(input, para_in)
+    elif process=='CLAHE':
+        output = clahe_fnc(input,para_in)
+    elif process=='Lpull':
+        output = Lpull_fnc(input,para_in)
+    elif process=='Downsample':
+        output = downsample_fnc(input,para_in)
+    else:
+        print('No process performed, returning input')
+        output=input
+    return output
+def preprocessing_roulette(input, process_para):
+    '''
+    Crop:
+    para={'crop size': 2048,
+          'i':0,
+          'j':0}
+
+    Gaussian:
+    para={'kernel size': 3}
+
+    CLAHE:
+    para={'clahe window': 50,
+            'clip limit': 4}
+
+    Lpull:
+    para={'thres': 60,
+          'pull': 60}
+
+    Downsample:
+    para={'fxy':2,
+          'inter_area': None}
+    '''
+    temp_input = input.copy()
+    for process, para in process_para.items():
+        temp_input=load_roulette(temp_input,process,para)
+    return temp_input
